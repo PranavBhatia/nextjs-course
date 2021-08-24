db309b5175d81426e9248c0c444b78f783db5571
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-08-23 23:26:46 -0400

    Navigating To Dynamic Routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "pb", name: "Pranav" },
+    { id: "ks", name: "Kritika" },
+    { id: "sb", name: "Saurabh" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
